704519633c29cbecd0c8b807f3aff4702b3ec399
Nikita Oltyan <76751355+nikitaoltyan@users.noreply.github.com>
2021-03-20 12:46:01 +0300

    Add gradient check

--- Assignment4/gradient_check.py
+++ Assignment4/gradient_check.py
@@ -24,19 +24,22 @@ def check_gradient(f, x, delta=1e-5, tol=1e-4):
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
     while not it.finished:
         ix = it.multi_index
-        analytic_grad_at_ix = analytic_grad[ix]
-        numeric_grad_at_ix = 0
+        analytic_grad_at_ix = analytic_grad[ix] 
+        x0 = x.copy()
+        x1 = x.copy()
+        x0[ix] += delta
+        x1[ix] -= delta
+        
+        numeric_grad_at_ix = (f(x0)[0] - f(x1)[0]) / (2 * delta)
+        print(analytic_grad_at_ix , numeric_grad_at_ix)
 
-        # TODO Copy from previous assignment
-        raise Exception("Not implemented!")
-
+         #TODO compute value of numeric gradient of f to idx
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
-            print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (
-                  ix, analytic_grad_at_ix, numeric_grad_at_ix))
+            print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (ix, analytic_grad_at_ix, numeric_grad_at_ix))
             return False
 
         it.iternext()
-
+    
     print("Gradient check passed!")
     return True
 
